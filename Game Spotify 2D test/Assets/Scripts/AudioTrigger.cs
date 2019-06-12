106bd78c9eaa93a70da4bd61c1124ec90c8236fe
Derpkovsky <hidde.kleinnagelvoort@student.hku.nl>
2019-06-12 18:35:23 +0200

    Initial commit

---
+++ Game Spotify 2D test/Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public string insertMusicName;
+    private bool playMusic;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playMusic = true;
+        }
+        else
+        {
+            playMusic = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playMusic == true)
+        {
+            FindObjectOfType<AudioManager>().Play(insertMusicName);
+        }
+    }
+}
---
+++ Game Spotify 2D test/Assets/Scripts/CursorMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMover : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed;
+
+    void Update()
+    {
+        //if (Input.GetMouseButton(1))
+        //{
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        //}
+    }
+}
